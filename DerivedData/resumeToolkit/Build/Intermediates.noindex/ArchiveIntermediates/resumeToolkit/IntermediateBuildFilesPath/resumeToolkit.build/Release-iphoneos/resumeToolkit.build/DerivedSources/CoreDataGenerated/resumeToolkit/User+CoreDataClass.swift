2e0b85e0f684590997440833cdc0ecac8f2dfefa
Sam Smith <sam@example.co>
2017-11-24 22:04:41 -0800

    pre configuration for ipad

---
+++ DerivedData/resumeToolkit/Build/Intermediates.noindex/ArchiveIntermediates/resumeToolkit/IntermediateBuildFilesPath/resumeToolkit.build/Release-iphoneos/resumeToolkit.build/DerivedSources/CoreDataGenerated/resumeToolkit/User+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  User+CoreDataClass.swift
+//  
+//
+//  Created by arya mirshafii on 11/15/17.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
